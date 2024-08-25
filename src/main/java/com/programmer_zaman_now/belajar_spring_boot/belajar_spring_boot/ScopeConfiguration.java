8dbcb70dae8074e0df442fd46afe3db84d5a2d12
fiqriturhamz <smandakotser@gmail.com>
2024-08-26 00:11:03 +0700

    membuat scope

--- src/main/java/com/programmer_zaman_now/belajar_spring_boot/belajar_spring_boot/ScopeConfiguration.java
+++ src/main/java/com/programmer_zaman_now/belajar_spring_boot/belajar_spring_boot/ScopeConfiguration.java
@@ -1,16 +1,33 @@
 package com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot;
 
+import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.data.Bar;
 import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.data.Foo;
-import lombok.extern.slf4j.Slf4j;
+import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.scope.DoubletonScope;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-@Slf4j
+
+
 @Configuration
 public class ScopeConfiguration {
     @Bean
     @Scope("prototype")
     public Foo foo() {
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        return new Bar();
+
     }
 }
--- src/test/java/com/programmer_zaman_now/belajar_spring_boot/belajar_spring_boot/ScopeTest.java
+++ src/test/java/com/programmer_zaman_now/belajar_spring_boot/belajar_spring_boot/ScopeTest.java
@@ -1,6 +1,8 @@
 package com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot;
 
+import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.data.Bar;
 import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.data.Foo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -8,15 +10,29 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ScopeTest {
     private ApplicationContext applicationContext;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         applicationContext = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
     }
 
     @Test
-    void testPrototypeScope(){
-       Foo foo1 = applicationContext.getBean(Foo.class);
-       Foo foo2 = applicationContext.getBean(Foo.class);
-       Foo foo3 = applicationContext.getBean(Foo.class);
+    void testPrototypeScope() {
+        Foo foo1 = applicationContext.getBean(Foo.class);
+        Foo foo2 = applicationContext.getBean(Foo.class);
+        Foo foo3 = applicationContext.getBean(Foo.class);
+    }
+
+    @Test
+    void testDoubleScope() {
+        Bar bar1 = applicationContext.getBean(Bar.class);
+        Bar bar2 = applicationContext.getBean(Bar.class);
+        Bar bar3 = applicationContext.getBean(Bar.class);
+        Bar bar4 = applicationContext.getBean(Bar.class);
+        Assertions.assertSame(bar1, bar3);
+        Assertions.assertSame(bar2, bar4);
+
+        Assertions.assertNotSame(bar1, bar2);
+        Assertions.assertNotSame(bar3, bar4);
     }
 }
