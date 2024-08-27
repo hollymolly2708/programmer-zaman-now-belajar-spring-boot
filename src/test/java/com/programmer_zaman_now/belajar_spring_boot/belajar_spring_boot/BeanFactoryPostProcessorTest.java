e4a687ecb11082b295c4377b92c41720647d9bc5
fiqriturhamz <smandakotser@gmail.com>
2024-08-27 15:01:41 +0700

    bean factory post processor

---
+++ src/test/java/com/programmer_zaman_now/belajar_spring_boot/belajar_spring_boot/BeanFactoryPostProcessorTest.java
@@ -0,0 +1,33 @@
+package com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot;
+
+import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.data.Foo;
+import com.programmer_zaman_now.belajar_spring_boot.belajar_spring_boot.processor.FooBeanFactoryPostProcessor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+public class BeanFactoryPostProcessorTest {
+    @Configuration
+    @Import(FooBeanFactoryPostProcessor.class)
+    public static class TestConfiguration {
+
+    }
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(TestConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testBeanFactoryPostProcessor() {
+        Foo foo = applicationContext.getBean(Foo.class);
+        Assertions.assertNotNull(foo);
+    }
+}
